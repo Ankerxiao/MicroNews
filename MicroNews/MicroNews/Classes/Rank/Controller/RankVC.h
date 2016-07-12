a20a727022d75023d1f2ae660229f36cfbea8348
Ankerxiao <807132689@qq.com>
2016-07-12 21:25:47 +0800

    7-12-01

--- MicroNews/MicroNews/Classes/Rank/Controller/RankVC.h
+++ MicroNews/MicroNews/Classes/Rank/Controller/RankVC.h
@@ -8,6 +8,10 @@
 
 #import "BaseVC.h"
 
+#define CUANHONG_API @"http://www.weixinkd.com/v3/article/top/get.json?ad=1&device_type=1&uid=13737897&access=WIfI&version_code=303&type=1&os_version=9.2.1&device_brand=%E8%8B%B9%E6%9E%9C&device_model=iPad&page=1&catid=0&device_platform=iphone&sign=39e9fe2d5fa83e52ddee572ff304610d&app_version=3.0.3&channel_code=80000000&openudid=9c0e321e1f550df652f08a74525b6c88&uuid=66A8DC9A-A153-4253-A7FB-DF3C1291580A"
+#define QITIAN_API @"http://www.weixinkd.com/v3/article/top/get.json?ad=1&device_type=1&uid=13737897&access=WIfI&version_code=303&type=2&os_version=9.2.1&device_brand=%E8%8B%B9%E6%9E%9C&device_model=iPad&page=1&catid=0&device_platform=iphone&sign=0f9999d05ced755ade8f4ec208a3f593&app_version=3.0.3&channel_code=80000000&openudid=9c0e321e1f550df652f08a74525b6c88&uuid=D2C098A9-21AB-4939-8C92-26151D131EF6"
+#define ZONGBANG_API @"http://www.weixinkd.com/v3/article/top/get.json?ad=1&device_type=1&uid=13737897&access=WIfI&version_code=303&type=3&os_version=9.2.1&device_brand=%E8%8B%B9%E6%9E%9C&device_model=iPad&page=1&catid=0&device_platform=iphone&sign=e49a22bb92f3019e8c3af3775611842b&app_version=3.0.3&channel_code=80000000&openudid=9c0e321e1f550df652f08a74525b6c88&uuid=DCD86B62-1FF9-4097-9D33-5023F21BB856"
+
 @interface RankVC : BaseVC
 
 @end
